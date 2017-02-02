dba7afa36d883fc1577aef53dc42ca7ba9d6819c
Ruben-Garcia <rubenga@gmail.com>
2017-02-02 21:12:12 +0100

    adding modules

--- src/main/java/com/udemy/component/RequestTimeInterceptor.java
+++ src/main/java/com/udemy/component/RequestTimeInterceptor.java
@@ -1,17 +1,29 @@
 package com.udemy.component;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.udemy.repository.LogRepository;
 
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
 
 	private static final Log log = LogFactory.getLog(RequestTimeInterceptor.class);
+	
+	@Autowired
+	@Qualifier("logRepository")
+	LogRepository logRepository;
 	
 	//Primero
 	@Override
@@ -26,7 +38,26 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		long startTime = (long) request.getAttribute("startTime");
-		log.info("Url to: '" + request.getRequestURL().toString() + "' in: " + (System.currentTimeMillis() - startTime));
+		String url = request.getRequestURL().toString();
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		String username = "";
+		
+		if (auth != null && auth.isAuthenticated()){
+			username = auth.getName();
+		}
+		
+		com.udemy.entity.Log ulog = new com.udemy.entity.Log(
+				new Date(),
+				auth.getDetails().toString(),
+				username, 
+				url);
+		
+		logRepository.save(ulog);
+
+		log.info("Url to: '" + url + "' in: " + (System.currentTimeMillis() - startTime));
+
 	}
 
 
